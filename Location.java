8926d0a4c66da3f32d8ec8e5b2cfab5bb12144b4
Jackson Jenks <jacksonjenks2002@gmail.com>
2025-01-17 15:16:35 -0800

    Added getter and setter method along with a constructor for the Max Value LAB.

--- Location.java
+++ Location.java
@@ -1,3 +1,45 @@
 public class Location {
+
+    private int row = 0;
+    private int column = 0;
+    private double maxValue = 0;
+
+    private int getRow() {
+
+        return this.row;
+
+    }
+
+    private int getColumn() {
+
+        return this.column;
+
+    }
+    private double getMaxValue() {
+
+        return this.maxValue;
+
+    }
+
+    private void setRow(int newRow) {
+
+        this.row = newRow;
     
+    }
+    
+    private void setcolumn(int newColumn) {
+
+        this.column = newColumn;
+    
+    }
+    private void setMaxValue(double newMaxValue) {
+
+        this.maxValue = newMaxValue;
+    
+    }
+
+    public Location() {
+
+    }
+
 }
